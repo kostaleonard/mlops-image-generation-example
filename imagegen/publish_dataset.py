6f259c0f1e9f86738f46725adb99c027967a16e8
Leo Kosta <kostaleonard@gmail.com>
2021-12-28 10:30:02 -0500

    Added dataset publication
    Signed-off-by: Leo Kosta <kostaleonard@gmail.com>

---
+++ imagegen/publish_dataset.py
@@ -0,0 +1,33 @@
+"""Publishes a new dataset to the local or remote filesystem. This script should
+be run any time the data processor changes."""
+
+from mlops.dataset.versioned_dataset_builder import VersionedDatasetBuilder
+from imagegen.pokemon_generation_data_processor import \
+    PokemonGenerationDataProcessor, DEFAULT_DATASET_PATH
+
+DATASET_VERSION = 'v1'
+DATASET_PUBLICATION_PATH_LOCAL = 'datasets'
+DATASET_PUBLICATION_PATH_S3 = \
+    's3://kosta-mlops/mlops-image-generation-example/datasets'
+TAGS = ['image', 'generation']
+
+
+def publish_dataset(publication_path: str) -> str:
+    """Builds and publishes the dataset.
+
+    :param publication_path: The path on the local or remote filesystem to which
+        to publish the dataset.
+    :return: The versioned dataset's publication path.
+    """
+    processor = PokemonGenerationDataProcessor()
+    builder = VersionedDatasetBuilder(DEFAULT_DATASET_PATH, processor)
+    return builder.publish(publication_path, DATASET_VERSION, tags=TAGS)
+
+
+def main() -> None:
+    """Runs the program."""
+    publish_dataset(DATASET_PUBLICATION_PATH_LOCAL)
+
+
+if __name__ == '__main__':
+    main()
--- tests/test_publish_dataset.py
+++ tests/test_publish_dataset.py
@@ -1,7 +1,17 @@
 """Tests publish_dataset.py."""
 
+import os
+import shutil
+from imagegen.publish_dataset import publish_dataset
 
-def test_placeholder() -> None:
-    """Placeholder test indicating that a module has not been tested."""
-    # TODO
-    assert False
+TEST_PUBLICATION_PATH_LOCAL = '/tmp/test_publish_dataset/datasets'
+
+
+def test_publish_dataset_creates_files() -> None:
+    """Tests that publish_dataset creates the dataset files."""
+    try:
+        shutil.rmtree(TEST_PUBLICATION_PATH_LOCAL)
+    except FileNotFoundError:
+        pass
+    publish_dataset(TEST_PUBLICATION_PATH_LOCAL)
+    assert os.path.exists(TEST_PUBLICATION_PATH_LOCAL)
